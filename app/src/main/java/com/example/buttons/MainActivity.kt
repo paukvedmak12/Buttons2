fb3729973a734abeef8e460a88045ed16022e271
paukvedmak <115324242+paukvedmak@users.noreply.github.com>
2023-10-06 17:31:46 +0300

    Created button and textview

--- app/src/main/java/com/example/buttons/MainActivity.kt
+++ app/src/main/java/com/example/buttons/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.buttons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+            textView.text = "Button Clicked!"
+        }
     }
+
 }
